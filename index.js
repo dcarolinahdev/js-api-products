469e25b0939d4cae84d0de8bc2fcab193d2edcd4
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2025-08-03 17:09:23 -0500

    Updating to first reading of data

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import fs from "fs";
 
 const app = express();
+const readData = () => {
+    const data = fs.readFileSync("./db.json");
+    return JSON.parse(data);
+};
+
+readData();
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my first api with nodejs !")
+});
 
 app.listen(3000, ()=> {
     console.log('Server listening on port 3000.')
